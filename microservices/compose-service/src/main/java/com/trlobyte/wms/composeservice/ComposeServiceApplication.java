f433b7b2cbcd89b1a74cd07a85a0d8e997519bcd
Jakub Kusza <jakub.kuszabm@gmail.com>
2024-01-17 11:39:45 +0100

    #1.1 ComponentScan

--- microservices/compose-service/src/main/java/com/trlobyte/wms/composeservice/ComposeServiceApplication.java
+++ microservices/compose-service/src/main/java/com/trlobyte/wms/composeservice/ComposeServiceApplication.java
@@ -2,8 +2,11 @@ package com.trlobyte.wms.composeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("org.kusza")
+@ComponentScan("com.trlobyte")
 public class ComposeServiceApplication {
 
 	public static void main(String[] args) {
