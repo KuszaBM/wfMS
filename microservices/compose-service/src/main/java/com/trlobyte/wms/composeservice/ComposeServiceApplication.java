4c88935e8bd372764507d01e48e71b7350d9153b
Jakub Kusza <jakub.kuszabm@gmail.com>
2024-01-17 13:04:18 +0100

    #1.5 Added RestTemplate Bean

--- microservices/compose-service/src/main/java/com/trlobyte/wms/composeservice/ComposeServiceApplication.java
+++ microservices/compose-service/src/main/java/com/trlobyte/wms/composeservice/ComposeServiceApplication.java
@@ -2,13 +2,19 @@ package com.trlobyte.wms.composeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("org.kusza")
 @ComponentScan("com.trlobyte")
 public class ComposeServiceApplication {
 
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ComposeServiceApplication.class, args);
 	}
