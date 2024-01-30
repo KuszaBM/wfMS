6605cca25cdf0a034634f1abd9def7a0bf3a085d
Jakub Kusza <jakub.kuszabm@gmail.com>
2024-01-30 07:56:54 +0100

    #7.2 StoragePlace repository added

---
+++ microservices/storage-service/src/main/java/com/trlobyte/wms/storageservice/repositories/StoragePlaceRepository.java
@@ -0,0 +1,6 @@
+package com.trlobyte.wms.storageservice.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface StoragePlaceRepository extends CrudRepository <StoragePlaceEntity, Integer> {
+}
